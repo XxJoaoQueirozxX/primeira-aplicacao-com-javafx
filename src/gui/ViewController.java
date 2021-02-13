ff46e128729dc60da3cf5e65bef14d4a525d1b5f
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-02-13 19:30:30 -0300

    text-fields, labels and simple sum app

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -5,6 +5,10 @@ import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class ViewController {
 
@@ -12,13 +16,31 @@ public class ViewController {
     private Button btTest;
 
     @FXML
+    private TextField field1;
+
+    @FXML
+    private TextField field2;
+
+    @FXML
+    private Label result;
+
+
+    @FXML
     public void onBtTestAction(){
-        Alerts.showAlert(
-                "Titulo",
-                null,
-                "Hello",
-                AlertType.ERROR
-        );
+        Locale.setDefault(Locale.US);
+        try{
+
+            Double sum = Double.parseDouble(field1.getText()) + Double.parseDouble(field2.getText());
+            result.setText(String.format("Result = %.2f", sum));
+        }catch (Exception e){
+            Alerts.showAlert(
+                    "Error",
+                    null,
+                    e.getMessage(),
+                    AlertType.ERROR
+            );
+        }
+
     }
 
 }
