124f1a0728dc8ced8c4b943dcb2eff9185c4de95
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2021-02-13 22:38:11 -0300

    combobox

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -3,54 +3,67 @@ package gui;
 
 import gui.util.Alerts;
 import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import model.entities.Person;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ViewController implements Initializable {
 
     @FXML
-    private Button btTest;
-
-    @FXML
-    private TextField field1;
-
-    @FXML
-    private TextField field2;
-
-    @FXML
-    private Label result;
+    private ComboBox<Person> comboBoxPerson;
+    private ObservableList<Person> obsList;
 
 
     @FXML
-    public void onBtTestAction(){
-        Locale.setDefault(Locale.US);
-        try{
+    private Button btAll;
 
-            Double sum = Double.parseDouble(field1.getText()) + Double.parseDouble(field2.getText());
-            result.setText(String.format("Result = %.2f", sum));
-        }catch (Exception e){
-            Alerts.showAlert(
-                    "Error",
-                    null,
-                    e.getMessage(),
-                    AlertType.ERROR
-            );
+    @FXML
+    public void onComboBoxPersonAction(){
+        Person p = comboBoxPerson.getSelectionModel().getSelectedItem();
+        System.out.println(p);
+    }
+
+    @FXML
+    public void onBtAllAction(){
+        for (Person p : comboBoxPerson.getItems()){
+            System.out.println(p);
         }
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<Person> list = new ArrayList<>();
 
-        Constraints.setTextFieldDouble(this.field1);
-        Constraints.setTextFieldDouble(this.field2);
+        list.add(new Person(1, "Maria", "maria@gmail.com"));
+        list.add(new Person(2, "Alex", "alex@gmail.com"));
+        list.add(new Person(3, "Bob", "bob@gmail.com"));
+
+        obsList = FXCollections.observableArrayList(list);
+
+        comboBoxPerson.setItems(obsList);
+
+
+        Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+            @Override
+            protected void updateItem(Person item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty ? "" : item.getName());
+            }
+        };
+        comboBoxPerson.setCellFactory(factory);
+        comboBoxPerson.setButtonCell(factory.call(null));
     }
+
+
 }
